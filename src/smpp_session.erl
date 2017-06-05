979effb960b11763b3cad9e75289a65111618ccd
s.budaev <s.budaev@fun-box.ru>
2017-06-05 12:10:47 +0400

    Typo fix

--- src/smpp_session.erl
+++ src/smpp_session.erl
@@ -222,7 +222,7 @@ handle_accept(Pid, Sock, ProxyIpList) ->
     case inet:peername(Sock) of
         {ok, {Addr, _Port}} ->
             lager:debug("sync_send_event accept to pid:~p Addr:~p, proxy_ip_list: ~p", [Pid, Addr, ProxyIpList]),
-            case lists:meber(Addr, ProxyIpList) of
+            case lists:member(Addr, ProxyIpList) of
                 true -> case proxy_protocol:accept(Sock) of
                             {ok, {proxy_opts, _IpVersion, SourceAddress, DestAddress, SourcePort, DestPort, ConnectionInfo}} ->
                                 gen_fsm:sync_send_event(Pid,
