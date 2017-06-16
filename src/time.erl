e477bac1611f1771e2a5b601849f6e2cc6bb734c
s.budaev <s.budaev@fun-box.ru>
2017-06-16 17:39:22 +0400

    Typo fix

--- src/time.erl
+++ src/time.erl
@@ -6,6 +6,6 @@ timestamp() ->
    OtpRelease = list_to_integer(erlang:system_info(otp_release)),
    case OtpRelease of
       17 -> erlang:now();
-      n when n >= 18 -> erlang:timestamp();
+      N when N >= 18 -> erlang:timestamp();
       _ -> {error, "OTP verstion is not supported"}
    end.
